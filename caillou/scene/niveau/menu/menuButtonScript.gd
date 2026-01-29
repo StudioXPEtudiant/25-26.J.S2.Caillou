e058d408368f3243d0af61598f7c7fc44c9d4d6f
clément <MiaouLaChate@gmail.com>
2026-01-29 19:46:45 +0100

    bouton menu marche

--- caillou/scene/niveau/menu/menuButtonScript.gd
+++ caillou/scene/niveau/menu/menuButtonScript.gd
@@ -1,18 +1,15 @@
 extends Button
 
-var button : Button = get
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	button_down.connect(buttonChecker)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Button.ACTION_MODE_BUTTON_PRESS == true :
-		buttonChecker()
+	pass
 
 
 func buttonChecker() :
-	if button.name == menu_button_play :
+	if name == "menu_button_play" :
 		pass
